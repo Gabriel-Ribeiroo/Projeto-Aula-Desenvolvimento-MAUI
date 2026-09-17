185bf870ceb4740c20b5eb12014084b52ddd5169
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2026-09-17 17:53:13 -0300

    feat: add AppearancePage

--- Projeto/AppShell.xaml.cs
+++ Projeto/AppShell.xaml.cs
@@ -5,6 +5,14 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(AppearencePage), typeof(AppearencePage)); 
+        }
+
+        private async void OnAppearencePageClicked(object sender, EventArgs args)
+        {
+            FlyoutIsPresented = false;
+            await GoToAsync(nameof(AppearencePage)); 
         }
     }
 }
---
+++ Projeto/AppearencePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Projeto;
+
+public partial class AppearencePage : ContentPage
+{
+	public AppearencePage()
+	{
+		InitializeComponent();
+    }
+}
